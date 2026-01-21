21aa032a9e95076b55670b3c4123366159f9bfb5
Giselle Hoekveld <gisellehoekveld@gmail.com>
2026-01-20 22:52:07 -0300

    Add configuration files for Prettier and ESLint, and set up TypeScript support for CSS modules

---
+++ @types/rocketseat-eslint-config.d.ts
@@ -0,0 +1,1 @@
+declare module '@rocketseat/eslint-config/next.js';
---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  semi: false,
+  singleQuote: true,
+  arrowParens: 'avoid',
+  trailingComma: 'all',
+  endOfLine: 'auto',
+}
