f8cbaa5ebfb6ca994fedd7d4c64a7767b3ba52aa
Giselle Hoekveld <gisellehoekveld@gmail.com>
2026-02-16 20:27:24 -0300

    Add ScrollArea component for horizontal tab scrolling; refactor SettingsTabs for improved layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,9 +21,9 @@ export default function RootLayout({
   return (
     <html lang="en" className="antialiased">
       <body className={`${inter.variable} antialiased`}>
-        <div className="grid-cols-app grid min-h-screen">
+        <div className="lg:grid-cols-app min-h-screen lg:grid">
           <Sidebar />
-          <main className="px-4 pt-24 pb-12 lg:col-start-2 lg:px-8 lg:pt-8 lg:pb-12">
+          <main className="max-w-[100vw] px-4 pt-24 pb-12 lg:col-start-2 lg:px-8 lg:pt-8">
             {children}
           </main>
         </div>
